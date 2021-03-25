4645999d2057a2a22bb79be1cd60ae17272e254d
Edith Molda <edith.22.mary@gmail.com>
2021-03-24 21:02:27 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,117 @@
+document.getElementById("1").addEventListener("click", mark1);
+document.getElementById("2").addEventListener("click", mark2);
+document.getElementById("3").addEventListener("click", mark3);
+document.getElementById("4").addEventListener("click", mark4);
+document.getElementById("5").addEventListener("click", mark5);
+document.getElementById("6").addEventListener("click", mark6);
+document.getElementById("7").addEventListener("click", mark7);
+document.getElementById("8").addEventListener("click", mark8);
+document.getElementById("9").addEventListener("click", mark9);
+
+function mark1() {
+    if (document.getElementById("1").innerHTML === "") {
+        document.getElementById("1").innerHTML = "X";
+    }
+    else if (document.getElementById("1").innerHTML === "X") {
+        document.getElementById("1").innerHTML = "O";
+    }
+    else if (document.getElementById("1").innerHTML === "O") {
+        document.getElementById("1").innerHTML = "";
+    }
+}
+
+function mark2() {
+    if (document.getElementById("2").innerHTML === "") {
+        document.getElementById("2").innerHTML = "X";
+    }
+    else if (document.getElementById("2").innerHTML === "X") {
+        document.getElementById("2").innerHTML = "O";
+    }
+    else if (document.getElementById("2").innerHTML === "O") {
+        document.getElementById("2").innerHTML = "";
+    }
+}
+
+function mark3() {
+    if (document.getElementById("3").innerHTML === "") {
+        document.getElementById("3").innerHTML = "X";
+    }
+    else if (document.getElementById("3").innerHTML === "X") {
+        document.getElementById("3").innerHTML = "O";
+    }
+    else if (document.getElementById("3").innerHTML === "O") {
+        document.getElementById("3").innerHTML = "";
+    }
+}
+
+function mark4() {
+    if (document.getElementById("4").innerHTML === "") {
+        document.getElementById("4").innerHTML = "X";
+    }
+    else if (document.getElementById("4").innerHTML === "X") {
+        document.getElementById("4").innerHTML = "O";
+    }
+    else if (document.getElementById("4").innerHTML === "O") {
+        document.getElementById("4").innerHTML = "";
+    }
+}
+
+function mark5() {
+    if (document.getElementById("5").innerHTML === "") {
+        document.getElementById("5").innerHTML = "X";
+    }
+    else if (document.getElementById("5").innerHTML === "X") {
+        document.getElementById("5").innerHTML = "O";
+    }
+    else if (document.getElementById("5").innerHTML === "O") {
+        document.getElementById("5").innerHTML = "";
+    }
+}
+
+function mark6() {
+    if (document.getElementById("6").innerHTML === "") {
+        document.getElementById("6").innerHTML = "X";
+    }
+    else if (document.getElementById("6").innerHTML === "X") {
+        document.getElementById("6").innerHTML = "O";
+    }
+    else if (document.getElementById("6").innerHTML === "O") {
+        document.getElementById("6").innerHTML = "";
+    }
+}
+
+function mark7() {
+    if (document.getElementById("7").innerHTML === "") {
+        document.getElementById("7").innerHTML = "X";
+    }
+    else if (document.getElementById("7").innerHTML === "X") {
+        document.getElementById("7").innerHTML = "O";
+    }
+    else if (document.getElementById("7").innerHTML === "O") {
+        document.getElementById("7").innerHTML = "";
+    }
+}
+
+function mark8() {
+    if (document.getElementById("8").innerHTML === "") {
+        document.getElementById("8").innerHTML = "X";
+    }
+    else if (document.getElementById("8").innerHTML === "X") {
+        document.getElementById("8").innerHTML = "O";
+    }
+    else if (document.getElementById("8").innerHTML === "O") {
+        document.getElementById("8").innerHTML = "";
+    }
+}
+
+function mark9() {
+    if (document.getElementById("9").innerHTML === "") {
+        document.getElementById("9").innerHTML = "X";
+    }
+    else if (document.getElementById("9").innerHTML === "X") {
+        document.getElementById("9").innerHTML = "O";
+    }
+    else if (document.getElementById("9").innerHTML === "O") {
+        document.getElementById("9").innerHTML = "";
+    }
+}
